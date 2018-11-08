826b367e4559de1fe147277dee85f8efb7de28ce
123 <123@163.com>
2018-11-08 17:42:36 +0800

    提交项目

---
+++ src/main/java/com/itheima/test/test_git.java
@@ -0,0 +1,17 @@
+package com.itheima.test;
+
+/**
+ * 测试在ider中使用git
+ */
+
+public class test_git {
+    public static void main(String[] args) {
+
+
+        System.out.println("holle git!!!");
+
+
+
+
+    }
+}
